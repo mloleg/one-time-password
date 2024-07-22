b555a1889e7818fb88dc3434eb97fa8c7276bef0
mloleg <ml.olegka@yandex.ru>
2024-07-22 15:45:12 +0300

    Initial commit

---
+++ src/main/java/ru/mloleg/onetimepassword/OneTimePasswordApplication.java
@@ -0,0 +1,11 @@
+package ru.mloleg.onetimepassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTimePasswordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneTimePasswordApplication.class, args);
+    }
+}
